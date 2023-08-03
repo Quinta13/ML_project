7ac7734a2bc414a007423086bd054eeb70ecbaf9
Quinta13 <878500@stud.unive.it>
2023-08-03 17:20:11 +0200

    settings for data preparation

--- settings.py
+++ settings.py
@@ -7,7 +7,14 @@ from typing import Dict, List, Tuple
 # Dataset download
 FREIHAND_URL: str = "https://lmb.informatik.uni-freiburg.de/data/freihand/FreiHAND_pub_v2.zip"
 FREIHAND_DIR: str = "FreiHAND"
+
 IMG_EXT: str = "jpg"
+ORIGINAL_SIZE: int = 224
+NEW_SIZE: int = 128
+
+RAW = 32560
+AUGMENTED = 3
+TOT_IMG = RAW * (AUGMENTED + 1)
 
 # Training
 TRAINING: str = path.join("training", "rgb")
@@ -15,8 +22,13 @@ TRAINING_3D: str = "training_xyz.json"
 TRAINING_2D: str = "training_xy.json"
 TRAINING_CAMERA: str = "training_K.json"
 
-# Test
-TEST: str = path.join("evaluation", "rgb")
+
+# Train - Test - Validation
+DATA = 1000
+
+TRAIN_PRC = 0.7
+TEST_PRC = .1
+VAL_PRC = .2
 
 # Logging
 LOG: bool = True
@@ -33,18 +45,20 @@ FINGERS = [THUMB, INDEX, MIDDLE, RING, LITTLE]
 
 # Finger colors
 COLORS: Dict[str, str] = {
-    THUMB: "#ed8671",
-    INDEX: "#bcd196",
-    MIDDLE: "#d9759a",
-    RING: "#eec56b",
-    LITTLE: "#c0b8c6"
+    THUMB: "ED8671",
+    INDEX: "BCD196",
+    MIDDLE: "D9759A",
+    RING: "EEC56B",
+    LITTLE: "C0B8C6"
 }
 
 # Finger connections
-CONNECTIONS: Dict[str, List[Tuple[int, int]]] = {
+WIDTH = 3
+
+LINES: Dict[str, List[Tuple[int, int]]] = {
     THUMB: [(0, 1), (1, 2), (2, 3), (3, 4)],
     INDEX: [(0, 5), (5, 6), (6, 7), (7, 8)],
     MIDDLE: [(0, 9), (9, 10), (10, 11), (11, 12)],
     RING: [(0, 13), (13, 14), (14, 15), (15, 16)],
-    LITTLE: [(0, 17), (17, 18), (18, 19), (20, 21)],
+    LITTLE: [(0, 17), (17, 18), (18, 19), (19, 20)],
 }
